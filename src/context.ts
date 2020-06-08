5b60a5db4d6a94b60a994c7a0616ab5f9df6cdd0
Jason Etcovitch <jasonetco@github.com>
2020-06-07 20:18:26 -0400

    Add Context#pullRequest

--- src/context.ts
+++ src/context.ts
@@ -77,6 +77,35 @@ export class Context {
       ...this.repo,
       number: (payload.issue || payload.pull_request || payload).number
     }
+
+    if (payload.issue) {
+      // If it's an issue, the API expects issue_number
+      data.issue_number = payload.issue.number
+    } else if (payload.pull_request) {
+      // If it's a PR, the API expects pull_number
+      data.issue_number = payload.pull_request.number
+    } else {
+      // Otherwise default to the old behaviour for BC reasons
+      // Long term, this should be data.number = payload.number
+      data.number = (payload.issue || payload.pull_request || payload).number
+    }
+
+    return data
+  }
+
+  public get pullRequest () {
+    const payload = this.payload
+
+    const data: { [k: string]: any } = {
+      ...this.repo
+    }
+
+    if (payload.pull_request) {
+      // If it's a PR, the API expects pull_number
+      data.pull_number = payload.pull_request.number
+    }
+
+    return data
   }
 
   public get repo () {
